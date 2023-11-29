1455408fa0aab4e7cbecbda09956db94544b28f1
kaustavsarkar14 <sarkar.kaustav14@gmail.com>
2023-11-29 14:27:07 +0530

    test

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,8 +5,8 @@ const Navbar = () => {
   const {totalQty,totalPrice} = useContext(cartContext)
   return (
     <nav>useReducer
-        <h2 id='nav-cart-item-count' >Total items : {totalQty}</h2>
-        <h2 id='cart-total-amount' >Total amount : {totalPrice}</h2>
+        <h2 id='nav-cart-item-count' >{totalQty}</h2>
+        <h2 id='cart-total-amount' >{totalPrice}</h2>
     </nav>
   )
 }
