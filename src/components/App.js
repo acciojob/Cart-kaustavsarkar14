9be75c21ade3368cd8bc64f155a11b68749d09a8
kaustavsarkar14 <sarkar.kaustav14@gmail.com>
2023-11-29 14:14:16 +0530

    test

--- src/components/App.js
+++ src/components/App.js
@@ -8,24 +8,26 @@ const App = () => {
     return (
         <div id='main'>
             <Navbar />
-            <button id='clear-all-cart' onClick={()=>dispatch({type:"clearcart"})} >Clear Cart</button>
-            <div id="cart-items-list">
+            <button id='clear-all-cart' onClick={() => dispatch({ type: "clearcart" })} >Clear Cart</button>
+            <div>
                 {
-                    cart.length==0?
-                    <p>Cart is currently empty</p>
-                    :
-                    cart.map(item => {
-                        return (
-                            <div key={item.id} style={{ border: "1px solid black" }} >
-                                <h3>{item.name}</h3>
-                                <p id={"cart-item-price-"+item.id} >${item.price}</p>
-                                <button id={`increment-btn-${item.id}`} onClick={() => dispatch({ type: "increment", id: item.id })}>+</button>
-                                <p id={"cart-amount-"+item.id} >{item.qty}</p>
-                                <button id={`decrement-btn-${item.id}`} onClick={() => dispatch({ type: "decrement", id: item.id })}>-</button>
-                                <button onClick={() => dispatch({ type: "remove", id: item.id })} >Remove</button>
-                            </div>
-                        )
-                    })
+                    cart.length == 0 ?
+                        <p>Cart is currently empty</p>
+                        :
+                        <div id="cart-items-list">
+                            {cart.map(item => {
+                                return (
+                                    <div key={item.id} style={{ border: "1px solid black" }} >
+                                        <h3>{item.name}</h3>
+                                        <p id={"cart-item-price-" + item.id} >${item.price}</p>
+                                        <button id={`increment-btn-${item.id}`} onClick={() => dispatch({ type: "increment", id: item.id })}>+</button>
+                                        <p id={"cart-amount-" + item.id} >{item.qty}</p>
+                                        <button id={`decrement-btn-${item.id}`} onClick={() => dispatch({ type: "decrement", id: item.id })}>-</button>
+                                        <button id={"cart-item-remove-" + item.id} onClick={() => dispatch({ type: "remove", id: item.id })} >Remove</button>
+                                    </div>
+                                )
+                            })}
+                        </div>
                 }
             </div>
         </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import cartContext from '../context/cartContext'
 const Navbar = () => {
   const {totalQty,totalPrice} = useContext(cartContext)
   return (
-    <nav>Navbar
+    <nav>useReducer
         <h2 id='nav-cart-item-count' >{totalQty}</h2>
         <h2 id='cart-total-amount' >{totalPrice}</h2>
     </nav>
